bc542027b18cc15cac08a2a5689781bf261f59f4
trongtram1903 <tramtrong13@gmail.com>
2025-04-01 10:25:10 +0700

    model

---
+++ Course.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManagerStudent.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+
+        [Required, MaxLength(255)]
+        public string CourseName { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public int CreditHours { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+    }
+}
