ac4b6cd44e822cbd614b956d5802b38e0e459b01
minhdeptrai07 <165239673+minhdeptrai07@users.noreply.github.com>
2025-04-01 10:48:16 +0700

    code

---
+++ Facade/ICourseFacade.cs
@@ -0,0 +1,13 @@
+﻿using ManagerStudent.Models;
+
+namespace ManagerStudent.Facade
+{
+    public interface ICourseFacade
+    {
+        IEnumerable<Course> GetAllCourses();
+        Course GetCourseById(int id);
+        void AddCourse(Course course);
+        void UpdateCourse(Course course);
+        void DeleteCourse(int id);
+    }
+}
