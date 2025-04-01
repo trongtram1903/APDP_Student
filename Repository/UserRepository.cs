d65697e8f92dca3306f49bd200360f62f0ca65f0
trongtram1903 <tramtrong13@gmail.com>
2025-04-01 11:14:31 +0700

    Repo

---
+++ Repository/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using ManagerStudent.Data;
+using ManagerStudent.Models;
+
+namespace ManagerStudent.Repository
+{
+    namespace ManagerStudent.Repository
+    {
+        public class UserRepository : IUserRepository
+        {
+            private readonly ApplicationDbContext _context;
+
+            public UserRepository(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public void Add(User user)
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+            }
+
+            public User GetByUsername(string username)
+            {
+                return _context.Users.FirstOrDefault(u => u.Username == username);
+            }
+
+            public bool Exists(string username)
+            {
+                return _context.Users.Any(u => u.Username == username);
+            }
+
+        
+        }
+    }
+
+}
